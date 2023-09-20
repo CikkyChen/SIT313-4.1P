74c8b76866afccf2695fac95f9fa3d5a4d06879f
CikkyChen <141930136+CikkyChen@users.noreply.github.com>
2023-09-20 16:58:45 +1000

    Add files via upload

---
+++ my-app(2)/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Profile.css'
+export default function Navbar() {
+    return (
+        <>
+        <div className="profile" id='home'>
+           <div className="img">
+           </div>
+        </div>
+       </>
+    )
+}
