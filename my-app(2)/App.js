e151488388860461b65599e716a1a180ad88d65c
CikkyChen <141930136+CikkyChen@users.noreply.github.com>
2023-09-20 17:00:27 +1000

    Add files via upload

---
+++ my-app(2)/App.js
@@ -0,0 +1,36 @@
+import Navbar from "./Navbar/Navbar";
+import './App.css';
+import Profile from "./Profile/Profile";
+import Articles from "./Articles/Articles";
+import Tutorials from "./Tutorials/Tutorials";
+import Subscribe from "./Subscribe/Subscribe";
+import Footer from "./Footer/Footer";
+
+function App() {
+
+  return (
+    <>
+
+ 
+
+     
+
+    <Navbar/>
+
+    <Profile/>
+
+    <Articles/>
+
+    <Tutorials/>
+
+    <Subscribe/>
+
+    <Footer/>
+    </>
+  );
+
+}
+
+ 
+
+export default App;
